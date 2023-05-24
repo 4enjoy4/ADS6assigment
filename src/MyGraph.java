d944ec2b5f70962ed4dcb4e0dc5190fa682a9bcd
Maksat <220451@astanait.edu.kz>
2023-05-24 20:18:55 +0600

    .

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -1,2 +1,20 @@
+import java.util.LinkedList;
+
 public class MyGraph {
+    private int numOfVertices;
+    private LinkedList<Integer>[] adgList;
+    public MyGraph(int numOfVertices){
+        this.numOfVertices = numOfVertices;
+        adgList = new LinkedList[numOfVertices];
+        for(int i = 0; i < numOfVertices; i++){
+            adgList[i] = new LinkedList<>();
+        }
+    }
+
+    private void validateVertex(int index){
+        if(index < 0 || index > numOfVertices){
+            throw new IllegalArgumentException("Vertex "+ index + " is out of range");
+        }
+    }
+
 }
