0483a5eff3beaea576047a8edd819e608a8fd01b
Maksat <220451@astanait.edu.kz>
2023-05-29 08:40:57 +0600

    .

--- src/DijkstraSearch.java
+++ src/DijkstraSearch.java
@@ -7,14 +7,18 @@ public class DijkstraSearch<V> {
     private WeightedGraph<V> graph;
     private Map<Vertex<V>, Vertex<V>> previous;
 
+    // Constructs a DijkstraSearch object with the specified weighted graph.
     public DijkstraSearch(WeightedGraph<V> graph) {
         this.graph = graph;
         this.previous = new HashMap<>();
     }
+
+    // Finds the shortest paths from a source vertex to all other vertices in the graph using Dijkstra's algorithm.
     public Map<Vertex<V>, Double> shortestPaths(Vertex<V> source) {
         Map<Vertex<V>, Double> distance = new HashMap<>();
         PriorityQueue<DijkstraNode<V>> priorityQueue = new PriorityQueue<>(Comparator.comparingDouble(DijkstraNode::getDistance));
 
+        // Initialization
         for (Vertex<V> vertex : graph.getVertices()) {
             if (vertex.equals(source))
                 distance.put(vertex, 0.0);
@@ -24,6 +28,7 @@ public class DijkstraSearch<V> {
             priorityQueue.add(new DijkstraNode<>(vertex, distance.get(vertex)));
         }
 
+        // Dijkstra's algorithm
         while (!priorityQueue.isEmpty()) {
             DijkstraNode<V> current = priorityQueue.poll();
             Vertex<V> currentVertex = current.getVertex();
@@ -47,6 +52,8 @@ public class DijkstraSearch<V> {
 
         return distance;
     }
+
+    // Retrieves the shortest path from a source vertex to a destination vertex.
     public List<Vertex<V>> shortestPathTo(Vertex<V> source, Vertex<V> destination) {
         Map<Vertex<V>, Double> distance = shortestPaths(source);
         List<Vertex<V>> path = new ArrayList<>();
@@ -60,29 +67,35 @@ public class DijkstraSearch<V> {
 
         return path;
     }
+
     private class DijkstraNode<T> implements Comparable<DijkstraNode<T>> {
         private Vertex<T> vertex;
         private double distance;
 
+        // Constructs a DijkstraNode object with the specified vertex and distance.
         public DijkstraNode(Vertex<T> vertex, double distance) {
             this.vertex = vertex;
             this.distance = distance;
         }
 
+        // Returns the vertex associated with this DijkstraNode.
         public Vertex<T> getVertex() {
             return vertex;
         }
 
+        // Returns the distance from the source vertex to the associated vertex.
         public double getDistance() {
             return distance;
         }
 
+        // Compares this DijkstraNode with another DijkstraNode based on their distances.
         @Override
         public int compareTo(DijkstraNode<T> other) {
             return Double.compare(distance, other.distance);
         }
     }
 }
+
 
 
 
