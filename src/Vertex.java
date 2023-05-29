42496dbe51ef759a61daf10b9f1cff502a18071e
Maksat <220451@astanait.edu.kz>
2023-05-29 08:37:34 +0600

    .

--- src/Vertex.java
+++ src/Vertex.java
@@ -6,15 +6,16 @@ import java.util.Objects;
 
 public class Vertex<V> {
     private V data;
-
+    //Constructs a vertex with the specified data.
     public Vertex(V data) {
         this.data = data;
     }
-
+    //Returns the data stored in the vertex.
     public V getData() {
         return data;
     }
-
+    //Compares this vertex to the specified object. The result is true if and only if the argument is a Vertex object
+    //and both objects have equal data values.
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -22,12 +23,12 @@ public class Vertex<V> {
         Vertex<?> vertex = (Vertex<?>) o;
         return Objects.equals(data, vertex.data);
     }
-
+    //Returns a hash code value for the vertex based on its data value.
     @Override
     public int hashCode() {
         return Objects.hash(data);
     }
-
+    //Returns a string representation of the vertex, which is the string representation of its data.
     @Override
     public String toString() {
         return data.toString();
